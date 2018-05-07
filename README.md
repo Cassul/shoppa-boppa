b66c8a4eabd822501641dab50cfddc2104e70c89
Cassul <aysa.garden@gmail.com>
2018-05-07 13:59:36 +1000

    implementing set margin for all and individual cost price

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 # Shoppa-boppa - Shopify app (Cost Of Goods Sold Calculator)
 
 ## Technologies used: Ruby(2.4.3p205), ROR(5.1.5), Postgres
+
+### Embedded app to see it running you need to have a functioning Shopify store
--- app/controllers/product_variants_controller.rb
+++ app/controllers/product_variants_controller.rb
@@ -3,4 +3,19 @@ class ProductVariantsController < ApplicationController
   def index
     @prod = Product.where(shop_id: session.values[3]).paginate(:page => params[:page], :per_page => 10).order('sales DESC') 
   end
+
+  def create
+    if params["margin_all"]
+      product_variants = ProductVariant.all
+      product_variants.each do |v| 
+        v.cost = (v.order_items.last.price.to_f * (100 - params['margin_all'].to_f) / 100).to_s
+        v.save
+      end
+    end
+    if params["cost"]
+      product_variant = ProductVariant.find_by(variant_id: params["variant_id"])
+      product_variant.cost = (product_variant.order_items.last.price.to_f * (100 - params["cost"].to_f) / 100).to_s
+    end
+      redirect_to "/product_variants"
+  end
 end
